557e45ea08a3273a244c45cc238ea4c437010782
Francisco Pilosio <fjpilosio@gmail.com>
2022-08-09 21:42:02 +0200

    done_Iteration_1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 // Iteration 1: Names and Input
 //
-// Iteration 2: Conditionals
+let hacker1 = "The driver's name is XXXX";
 
+let hacker2 = "he navigator's name is YYYY";
+
+console.log(hacker1, hacker2);
+
+//console.log("I'm ready!");
+// Iteration 2: Conditionals
 
 // Iteration 3: Loops
