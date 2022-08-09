19399fbba6e5d7ce9b3971d921088c568ea06ce4
geripairo <gerardpairo@hotmail.com>
2022-08-09 21:49:22 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+console.log("I'm ready!");
 
+// const hacker1 = "Gerard";
 
-// Iteration 3: Loops
+// console.log(`The driver's name is ${hacker1}`)
+
+// const hacker2 = "Marcos";
+
+// console.log(`The navigator's name is ${hacker2}`);
+
+// if( hacker1.length > hacker2.length ){
+//   console.log(`The diver has the longest name, it has ${hacker1.length} characters`)
+// } else if (hacker2.length > hacker1.length){
+//   console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
+// } else if (hacker1.length === hacker2.length){
+//   console.log(`Wow, you both have equally long names, ${hacker1.length} characters`)
+// }
+
