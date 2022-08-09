f5911841fb3997493765ce53beb902e4f8caa22d
Viviana Velez Montoya <vivianavelezm@hotmail.com>
2022-08-09 21:52:08 +0200

    se crea variable hacker1 y hacker2 y se imprimen

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 // Iteration 1: Names and Input
 //
+let hacker1;
+hacker1=  "Pedro";
+
+console.log("The driver's name is "+ hacker1);
+
+let hacker2;
+hacker2= "Rosa";
+
+console.log ("The navigator's name is " + hacker2);
 // Iteration 2: Conditionals
 
 
