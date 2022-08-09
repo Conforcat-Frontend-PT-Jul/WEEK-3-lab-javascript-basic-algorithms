999f6cae3111035538d885f28ce06193e31b679b
Marga <margarita.benitez.lopez@gmail.com>
2022-08-09 22:37:19 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 // Iteration 1: Names and Input
 //
+const hacker1 = "Kurt";
+console.log("the driver's name is " + hacker1)
+const hacker2 = "Monika";
+console.log("the navigator's name is " + hacker2)
+
+
 // Iteration 2: Conditionals
 
 
