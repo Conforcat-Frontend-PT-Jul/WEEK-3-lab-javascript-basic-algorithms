710e8d05e4d7ff8993511702a2d715cff5f3c0ee
Lollo Windows <lorenzocorio@duck.com>
2022-08-09 21:31:38 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
+console.log("I'm ready!");
+
 // Iteration 1: Names and Input
-//
+
+const hacker1 = `Lorenzo`;
+
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2;
+
+hacker2 = `...still unknown`
+console.log(`The navigator's name is ${hacker2}`)
+
 // Iteration 2: Conditionals
 
 
