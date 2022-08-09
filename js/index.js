cb3300d315395fd03a0475b9544c44e05deac4d9
laggart <laggart@gmail.com>
2022-08-09 21:50:26 +0200

    Names and Inputs realizado

--- js/index.js
+++ js/index.js
@@ -4,3 +4,19 @@
 
 
 // Iteration 3: Loops
+
+//Iteration 1:----------------------------//
+
+let hacker1 = 'Juan Carlos';
+console.log('the driver is', hacker1);
+
+let hacker2 = 'Nadie'
+console.log('The navigator is', hacker2);
+
+////Iteration 2:----------------------------//
+
+if{
+
+}
+
+
