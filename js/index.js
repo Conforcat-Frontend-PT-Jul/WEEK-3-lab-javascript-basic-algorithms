1f6f59d39dd7caa72ceb082a66c2cebaed717c02
Vicente <vmedina@omitsis.com>
2022-08-09 21:39:35 +0200

    :tada: Done Iteration 1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 // Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
 
+var hacker1 = "Vicente";
+console.log("The driver's name is " + hacker1);
+var hacker2 = "Juan";
+console.log("The navigator's name is " + hacker2);
+
+// Iteration 2: Conditionals
 
 // Iteration 3: Loops
