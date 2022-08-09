0cf4e987beb8afb4a03b7e925cf1d67a71545457
Leandro Balaguer <leandro.balaguer@gmail.com>
2022-08-09 21:50:56 +0200

    done!

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
 // Iteration 1: Names and Input
+console.log("I´m ready!")
+
+let hacker1 = "Jack"
+console.log("The driver´s name is: ", hacker1)
+
+let hacker2 = "Bob"
+console.log("The navigator's name is:", hacker2)
 //
 // Iteration 2: Conditionals
 
+console.log(hacker1.length)
+console.log(hacker2.length)
+
+console.log("Looks like", hacker1, "has the longest name with", hacker1.length, "characters. Well donde fckr!")
 
 // Iteration 3: Loops
+
+console.log(hacker1.toUpperCase())
+
+console.log(hacker1.split("").reverse().join(""))
+
+// Iteration 4: Loops
