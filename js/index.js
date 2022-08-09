ce355787827f09e9d7af46840e6affc66c0e6ddc
mava <mavafiza@gmail.com>
2022-08-09 23:36:43 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 // Iteration 1: Names and Input
 //
+console.log ("I'm ready! ");
+let hacker1 = "XXX";
+console.log ("the driver's name is " + hacker1);
+let hacker2 = "YYY";
+console.log ("the navigator's name is " + hacker2);
+
 // Iteration 2: Conditionals
 
 
