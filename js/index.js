d4b2af542bafc828a3265e840fc89f97674124a6
Alicia Barrios <aliciabgo@gmail.com>
2022-08-09 21:25:28 +0200

    iteration 1 done

--- js/index.js
+++ js/index.js
@@ -2,6 +2,14 @@ console.log("I'm ready!");
 
 // Iteration 1: Names and Input
 
+let hacker1 = 'Alicia';
+
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = 'Sergio';
+
+console.log(`The driver's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
 
 
