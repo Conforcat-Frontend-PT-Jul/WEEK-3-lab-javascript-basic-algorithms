5db27b06be872e27763ac340f29b798e5e34f3bd
alelof <alejandra.lof@gmail.com>
2022-08-09 21:13:33 +0200

    first iteration done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+console.log("I'm ready!");
 
+// Iteration 1: Names and Input
+let hacker1 = "Driver";
+let hacker2 = "Navigator";
+console.log("The driver's name is "+ hacker1);
+console.log("The navigator's name is "+ hacker2);
+
+// Iteration 2: Conditionals
 
 // Iteration 3: Loops
