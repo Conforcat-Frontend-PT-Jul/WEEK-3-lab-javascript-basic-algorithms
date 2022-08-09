31074605d3aedb2fe24ada311947e3d25f605e4f
geripairo <gerardpairo@hotmail.com>
2022-08-09 21:54:36 +0200

    iteration 1 and iteration 2 finished

--- js/index.js
+++ js/index.js
@@ -1,18 +1,24 @@
+// IM READY TO START!
+
 console.log("I'm ready!");
 
-// const hacker1 = "Gerard";
+// ITERARION 1
 
-// console.log(`The driver's name is ${hacker1}`)
+const hacker1 = "Gerard";
 
-// const hacker2 = "Marcos";
+console.log(`The driver's name is ${hacker1}`)
 
-// console.log(`The navigator's name is ${hacker2}`);
+const hacker2 = "Marcos";
 
-// if( hacker1.length > hacker2.length ){
-//   console.log(`The diver has the longest name, it has ${hacker1.length} characters`)
-// } else if (hacker2.length > hacker1.length){
-//   console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
-// } else if (hacker1.length === hacker2.length){
-//   console.log(`Wow, you both have equally long names, ${hacker1.length} characters`)
-// }
+console.log(`The navigator's name is ${hacker2}`);
+
+// ITERATION 2
+
+if( hacker1.length > hacker2.length ){
+  console.log(`The diver has the longest name, it has ${hacker1.length} characters`)
+} else if (hacker2.length > hacker1.length){
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
+} else if (hacker1.length === hacker2.length){
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters`)
+}
 
