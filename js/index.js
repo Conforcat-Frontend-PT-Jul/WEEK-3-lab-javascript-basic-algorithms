f9a934051d05d17ba1b1dec5058c3a659517f22c
laura86bf <laura86bf@hotmail.com>
2022-08-10 17:03:08 +0200

    iteration1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 // Iteration 1: Names and Input
 //
+console.log ("I'm ready!");
+
+const hacker1 = "Manolo";
+console.log("The driver's name is " + hacker1 + ".");
+
+const hacker2 = "Google Chrome";
+console.log("The navigator's name is " + hacker2 + ".");
+
 // Iteration 2: Conditionals
 
 
 // Iteration 3: Loops
+
