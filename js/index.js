bbe16677aa8c90442fe61286bd9334043696f9f0
Alicia Barrios <aliciabgo@gmail.com>
2022-08-10 15:36:50 +0200

    Iteration 3.1 y 3.2 done

--- js/index.js
+++ js/index.js
@@ -21,3 +21,37 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+
+// 3.1. De la siguiente manera hubiera resuelto el ejercicio con un loop:
+
+let hacker1CapitalLetters = "";
+
+for (i=0; i < hacker1.length; i++) {
+  hacker1CapitalLetters += hacker1[i].toUpperCase() + " ";
+}
+
+console.log("Resultado con loop:", hacker1CapitalLetters);
+
+// 3.1. Me ha parecido más limpio hacerlo con las funciones split(), pasando el string a un array, y join() volviendo a pasar el array a string con espacio entre cada letra:
+
+let driverCapitalLetters = hacker1.toUpperCase().split("").join(" ");
+
+console.log("Resultado con funciones:", driverCapitalLetters);
+
+// 3.2. De la siguiente manera hubiera resuelto el ejercicio con un loop:
+
+let hacker2Reverse = "";
+
+for (i=0; i < hacker2.length; i++) {
+  hacker2Reverse += hacker2[(hacker2.length-1)-i];
+}
+
+console.log("Resultado con loop:", hacker2Reverse);
+
+// 3.2. La función reverse() devuelve un array ordenado del reves, posteriormente pasamos el array a string con la función join():
+
+let navigatorReverse = hacker2.split("").reverse().join("");
+
+console.log("Resultado con funciones:", navigatorReverse);
+
+// 3.3.
