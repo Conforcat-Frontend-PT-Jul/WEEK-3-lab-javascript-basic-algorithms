6ac5a189f8ad46b68eef73373ab5791330589885
Leandro Balaguer <leandro.balaguer@gmail.com>
2022-08-11 22:11:55 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,17 +7,35 @@ console.log("The driver´s name is: ", hacker1)
 let hacker2 = "Bob"
 console.log("The navigator's name is:", hacker2)
 //
-// Iteration 2: Conditionals
+// Iteration 2.1: Conditionals
 
 console.log(hacker1.length)
 console.log(hacker2.length)
 
-console.log("Looks like", hacker1, "has the longest name with", hacker1.length, "characters. Well donde fckr!")
+console.log("Looks like", hacker1, "has the longest name with", hacker1.length, "characters. Well done!")
 
 // Iteration 3: Loops
 
-console.log(hacker1.toUpperCase())
+// 3.1 
+console.log(hacker1.toUpperCase().split('').join(' '))
 
+// 3.2
 console.log(hacker1.split("").reverse().join(""))
 
-// Iteration 4: Loops
+// 3.3
+
+let names= [hacker1, hacker2]
+console.log(names.sort());
+
+let drivers = ["Jack", "Bob"];
+drivers.sort((a, b) => {
+  if (a == b) {
+    console.log("What?! You both have the same name?")
+  }
+  if (a < b) {
+    console.log("Yo, the navigator goes first definitely.")
+  }
+  if (a > b) {
+  console.log("The driver's name goes first.")
+}
+});
