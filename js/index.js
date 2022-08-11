f75bb5532fbe05f8f2b4bfb784280b448bc1000d
jspsegarra <jspsegarra86@gmail.com>
2022-08-11 21:52:58 +0200

    Corrección Itineration 1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-console.log("I'm ready!");
+let hacker1 = "Josep";
+    console.log(`The driver's name is ${hacker1}`);
+let hacker2 = "Maria";
+    console.log(`The navigator's name is ${hacker2}`);
+
+
+
+
+
 // Iteration 1: Names and Input
 //
 // Iteration 2: Conditionals
