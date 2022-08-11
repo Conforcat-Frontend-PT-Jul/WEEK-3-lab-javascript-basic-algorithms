c17dcb3214ea563b9d86d48c9cfdd9b82a447711
laura86bf <laura86bf@hotmail.com>
2022-08-11 23:32:43 +0200

    iteration 2 + 3.1

--- js/index.js
+++ js/index.js
@@ -10,6 +10,17 @@ console.log("The navigator's name is " + hacker2 + ".");
 
 // Iteration 2: Conditionals
 
-
+if(hacker1.legth>hacker2.length){
+    console.log(`The driver has the longest name, it has ${hacker1.length}characters. `);
+}
+else if(hacker1.length<hacker2.length){
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+}
+else{
+   console.log( `Wow, you both have equally long names, XX characters!`);
+}
 // Iteration 3: Loops
 
+for(i=0;i<hacker1.length,i++){
+    console.log(hacker[i].toUpperCase);
+}
