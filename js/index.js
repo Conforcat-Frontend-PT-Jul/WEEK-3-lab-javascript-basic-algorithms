9b1d2fe0c5b2ddd0bf803778d0df96a6ffd3cc16
laggart <laggart@gmail.com>
2022-08-11 23:59:21 +0200

    Iteration 2 y 3 ready

--- js/index.js
+++ js/index.js
@@ -15,8 +15,46 @@ console.log('The navigator is', hacker2);
 
 ////Iteration 2:----------------------------//
 
-if{
+let driverNameLength = hacker1.length
+let navigatorNameLength = hacker2.length
 
+if (driverNameLength > navigatorNameLength) { 
+    console.log('the driver has the longest name, it has,', driverNameLength, 'characters')
+} else if (driverNameLength < navigatorNameLength) {
+    console.log('it seems that the navigator has the longest name, it has', navigatorNameLength, 'characters!')
+} else {
+    console.log ('Wow, you both have equally long names,', driverNameLength, 'characters')
 }
 
+
+////Iteration 3:----------------------------//
+
+
+let upperHacker1 = hacker1.toUpperCase();
+
+let i=0
+while (i < driverNameLength){
+    console.log(upperHacker1[i], " ")
+    i++;
+}
+
+let space = ''
+    space = hacker1.toUpperCase().split('').join(' ');
+    console.log(space);
+    
+let reverseName = ''
+
+    reverseName = hacker1.split('').reverse().join('')
+    console.log(reverseName);
+
+if (hacker1 < hacker2){
+    console.log("The driver's name goes first." )
+} else if (hacker1 > hacker2){
+    console.log("Yo, the navigator goes first definitely.")
+} else {
+    console.log("What?! You both have the same name?")
+}
+
+
+ 
 
