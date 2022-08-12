2d084066d7b2562b9402fbbc90d13e46d83a9127
Marga <margarita.benitez.lopez@gmail.com>
2022-08-13 01:02:09 +0200

    done

--- js/index.js
+++ js/index.js
@@ -8,5 +8,25 @@ console.log("the navigator's name is " + hacker2)
 
 // Iteration 2: Conditionals
 
+function longestName(name1, name2) {
+    if (name1.length > name2.length) {
+        console.log("the driver has the longest name, it has " + name1.length + " characters")
+    } else if (name1.length < name2.length) {
+         console.log("It seems that the navigator has the longest name, it has " + name2.length + " characters") 
+    }
+    else 
+    console.log("Wow, you both have equally long names, " + name1.length + " characters")
+
+
+}
+longestName(hacker1, hacker2)
+
 
 // Iteration 3: Loops
+const separateNamesLetters = hacker1.split("").join(" ").toUpperCase();
+console.log(separateNamesLetters)
+const printReverse = hacker2.split("").reverse().join("");
+console.log(printReverse)
+function alphabeticOrder(name1, name2) {
+    if
+}
