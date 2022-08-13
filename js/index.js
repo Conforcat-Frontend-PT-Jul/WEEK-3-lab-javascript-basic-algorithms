289e8b9b6413dcef627842bfe72fc2128e61c3c5
FerJAlva <fernanjalvarez@gmail.com>
2022-08-13 13:00:11 +0200

    Iteracion1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
-//
+let hacker1= "John";
+console.log ("The driver's name is",hacker1);
+let hacker2= "Fer"
+console.log("the navigator's name is",hacker2)
+
 // Iteration 2: Conditionals
 
 
