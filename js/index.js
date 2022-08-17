59b5b3322cd65f86a8e1e28994f612ee29179733
kevin ibarra <kevinibarra76@gmail.com>
2022-08-17 22:12:16 +0200

    entrega ejercicio semana 3 de kevin

--- js/index.js
+++ js/index.js
@@ -1,8 +1,47 @@
 // Iteration 1: Names and Input
-console.log("i'm ready");
+let hacker1 = "kevin"; 
 
+console.log("The driver's name is "+ hacker1);
+
+let hacker2 = "chrome"; 
+
+console.log("The navigator's name is "+ hacker2);
 
 // Iteration 2: Conditionals
+let numCharacterDriver= hacker1.length; 
+let numCharacterNavigator= hacker2.length; 
 
+if (numCharacterDriver>numCharacterNavigator) {
+    console.log("The driver has the longest name, it has " +  numCharacterDriver + " characters.");
+} else if (numCharacterDriver<numCharacterNavigator){
+    console.log("It seems that the navigator has the longest name, it has " + numCharacterNavigator + " characters.");
+} else {
+    console.log("Wow, you both have equally long names, " + numCharacterDriver + " characters!");
+}
 
 // Iteration 3: Loops
+let driveNameSplit= hacker1.split("");
+let newName= ""; 
+
+driveNameSplit.forEach((value) => {
+    newName += value.toUpperCase() + " "; 
+});
+
+console.log(newName);
+
+let navigatorSplit= hacker2.split("");
+let newNavigator= ""; 
+
+navigatorSplit.reverse().forEach(value => {
+    newNavigator += value+ "";
+       
+});
+console.log(newNavigator);
+
+if (hacker1<hacker2) {
+    console.log("The driver's name goes first.");
+} else if (hacker1>hacker2) {
+    console.log("Yo, the navigator goes first definitely.");
+} else {
+    console.log("What?! You both have the same name?");
+}
